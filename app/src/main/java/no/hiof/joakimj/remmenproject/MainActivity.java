793f2df951a20d37f106ceca7f28ba9342a69f0a
Joakim <jjohansen@csumb.edu>
2018-10-02 14:43:48 +0200

    Some Fixes

--- app/src/main/java/no/hiof/joakimj/remmenproject/MainActivity.java
+++ app/src/main/java/no/hiof/joakimj/remmenproject/MainActivity.java
@@ -1,13 +1,78 @@
 package no.hiof.joakimj.remmenproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Database
+    private FirebaseDatabase mDatabase;
+
+    //Authenticator Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+
+        generateTestData();
+
+        //createAuthenticationListener();
+        //createDatabaseListener();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /*
+    private void createAuthenticationListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(FirebaseAuth mAuth) {
+                FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                if (firebaseUser == null) {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
+                                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+    */
+
+    /**
+     * Generating test data from Firebase Database.
+     *
+     * @param
+     *
+     */
+    private void generateTestData() {
+
     }
 }
