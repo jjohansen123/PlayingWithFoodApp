d669b02ec5c4890a9d9930d61f9294f0128569f7
Joakim <jjohansen@csumb.edu>
2018-10-17 18:03:32 +0200

    working Google and SIgnout

--- app/src/main/java/no/hiof/joakimj/remmenproject/MainActivity.java
+++ app/src/main/java/no/hiof/joakimj/remmenproject/MainActivity.java
@@ -3,16 +3,33 @@ package no.hiof.joakimj.remmenproject;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+
+    private ImageButton googleLoginBtn;
 
     //boolean firstImage = true;
 
@@ -25,17 +42,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.imageView);
         Button logoutBtn = findViewById(R.id.logOutBtn);
 
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AuthUI.getInstance().signOut(MainActivity.this);
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        createAuthenticationListener();
 
         AsyncTask.execute(new Runnable() {
-            @Override
+                @Override
             public void run() {
                 // All your networking logic
                 // should be here
@@ -63,11 +75,76 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Swipe Top", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        if(firebaseAuthStateListener != null) {
+            firebaseAuth.addAuthStateListener(firebaseAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(firebaseAuthStateListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthStateListener);
+        }
+    }
+
+    private void createAuthenticationListener() {
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser == null) {
+                    startActivityForResult(AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
+                                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
     }
 
 
+    //menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sign_out_item:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                Toast.makeText(this, "Signed in as " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 }
