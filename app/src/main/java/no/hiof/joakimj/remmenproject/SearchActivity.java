b4a309fdac7434bf22999161c3160a1d9333cf63
Joakim <jjohansen@csumb.edu>
2018-11-24 19:48:22 +0100

    sending intent to searchactivity

--- app/src/main/java/no/hiof/joakimj/remmenproject/SearchActivity.java
+++ app/src/main/java/no/hiof/joakimj/remmenproject/SearchActivity.java
@@ -1,13 +1,26 @@
 package no.hiof.joakimj.remmenproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SearchActivity extends AppCompatActivity {
+
+    TextView searchTextView;
+    String Query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Intent intent = getIntent();
+        String query = intent.getExtras().getString("Query");
+
+        Log.i("TAG", "Query: " + query);
     }
 }
